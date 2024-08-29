6eeabce84a892ca06e73ab8fe2627a0a4457e227
Alejandro Berrio <alejo26262013@gmail.com>
2024-08-28 21:07:42 -0500

    Creacion del proyecto

---
+++ Ejercicio_1/Ejercicio_1/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+
+        static void NewMethod()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+//AQUI ES DONDE VA TIRAR TODO EL CODIGO
